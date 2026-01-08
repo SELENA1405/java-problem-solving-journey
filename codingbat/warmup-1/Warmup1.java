d37da37cd6304820990e603f1aa8c8a82d52e76a
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-09 00:29:55 +0700

    Update Warmup1.java

--- codingbat/warmup-1/Warmup1.java
+++ codingbat/warmup-1/Warmup1.java
@@ -30,6 +30,21 @@ monkeyTrouble(true, false) → false
     } return false;
   }
 
+//sumDouble
+/*
+Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
+*/
+
+//My Solution 
+  public int sumDouble(int a, int b) {
+  if (a == b) {
+    return 2 * (a+b);
+  } return a + b;
+}
         // to be continued...
 }
 
