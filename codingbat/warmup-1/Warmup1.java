73250047cf9580584947bcc0ffd9a7d61027cf3b
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-09 00:38:40 +0700

    Update Warmup1.java

--- codingbat/warmup-1/Warmup1.java
+++ codingbat/warmup-1/Warmup1.java
@@ -1,6 +1,8 @@
 public class Warmup1 {
 
-//SleepIn Problem
+// =============================
+//    1. SleepIn Problem
+// =============================
 /*
 1. The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
 sleepIn(false, false) → true
@@ -14,8 +16,10 @@ sleepIn(false, true) → true
             return true;
         } return false;
   } 
-        
-//monkeyTrouble
+
+// =============================
+//     2. monkeyTrouble
+// =============================
 /*
 2. We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
 monkeyTrouble(true, true) → true
@@ -29,8 +33,10 @@ monkeyTrouble(true, false) → false
       return true;
     } return false;
   }
-
-//sumDouble
+  
+// =============================
+//       3. sumDouble
+// =============================
 /*
 3. Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 sumDouble(1, 2) → 3
@@ -44,8 +50,10 @@ sumDouble(2, 2) → 8
     return 2 * (a+b);
   } return a + b;
 }
-
-//diff21
+  
+// =============================
+//         4. diff21
+// =============================
 /*
 4. Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
 diff21(19) → 2
@@ -61,8 +69,10 @@ diff21(21) → 0
     return 21 - n;
   }
 }
-
-//parrotTrouble
+  
+// =============================
+//      5. parrotTrouble
+// =============================
 /*
 5. We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
 parrotTrouble(true, 6) → true
