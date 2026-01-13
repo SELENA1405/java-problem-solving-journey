24cc2721d22f36cad46969fd893a49a804fae424
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-13 22:44:50 +0700

    problem 21 solved

--- codingbat/warmup-1/Warmup1.java
+++ codingbat/warmup-1/Warmup1.java
@@ -224,6 +224,20 @@ public class Warmup1 {
   return (aTeen || bTeen) && (aTeen != bTeen);
 }
 
+// =============================
+//       21. delDel
+// =============================
+
+//My Solution 
+  public String delDel(String str) {
+  if ((str.length() <= 3) ){
+    return str;
+  } else if(str.substring(1, 4).equals("del")){
+    return str.substring(0,1) + str.substring(4, str.length());
+  } return str;
+}
+
+  
  //to be continued ...
   
 } 
