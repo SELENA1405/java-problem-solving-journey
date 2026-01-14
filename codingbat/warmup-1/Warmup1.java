748cf7e8c7ea464a8479309921f97dd1e24cf04f
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-14 23:09:24 +0700

    problem 28 solved

--- codingbat/warmup-1/Warmup1.java
+++ codingbat/warmup-1/Warmup1.java
@@ -332,11 +332,21 @@ public class Warmup1 {
 // =============================
 
 //My Solution 
+  public boolean stringE(String str) {
+  int count = 0; 
+  for (int i = 0; i < str.length(); i ++){
+    if (str.charAt(i) == 'e') { count++; }
+  }
+  if (count <= 3 && count > 0){
+    return true;
+  } return false;
+}
   
-
 // =============================
 //       29. close10
 // =============================
+
+//My Solution 
 
 // =============================
 //       30. close10
