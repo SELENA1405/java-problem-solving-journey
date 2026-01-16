e184c2fd646f7525ffb3533517cbb40cc427075c
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-16 23:05:39 +0700

    problem 2 solved

--- codingbat/warmup-2/Warmup2.java
+++ codingbat/warmup-2/Warmup2.java
@@ -13,8 +13,22 @@ public class Warmup2 {
 }
 
 // =============================
-//    2. 
+//    2. frontTimes
 // =============================
+
+//My Solution 
+  public String frontTimes(String str, int n) {
+  String result = "";
+  String temp ="";
+  if (str.length() > 3){
+    temp = str.substring(0, 3);
+  } else if (str.length() <= 3){
+    temp = str;
+  }
+  for (int i = 0; i < n; i++){
+    result += temp;
+  } return result;
+}
 
 // =============================
 //    3. 
