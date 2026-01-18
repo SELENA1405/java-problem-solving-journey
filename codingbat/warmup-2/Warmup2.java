480666bc792fb4e5a5eb3d6e3394172881b7f6ac
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-18 15:27:35 +0700

    problem 10 solved

--- codingbat/warmup-2/Warmup2.java
+++ codingbat/warmup-2/Warmup2.java
@@ -125,7 +125,16 @@ public class Warmup2 {
   } return false;
 }
 
+// =============================
+//       10. array123
+// =============================
 
+//My Solution 
+  public boolean array123(int[] nums) {
+  for (int i = 0; i < nums.length-2; i++){
+    if (nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3) return true;
+  } return false;
+}
 
 // to be continued ...
   
