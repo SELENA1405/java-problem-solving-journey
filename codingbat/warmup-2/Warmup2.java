bc3fa2058508595615c3af5143e8d7b139675dc4
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-18 15:23:23 +0700

    problem 6 solved

--- codingbat/warmup-2/Warmup2.java
+++ codingbat/warmup-2/Warmup2.java
@@ -73,6 +73,19 @@ public class Warmup2 {
   } return result;
 }
 
+// =============================
+//       6. stringSplosion
+// =============================
+
+//My Solution 
+  public String stringSplosion(String str) {
+  String result ="";
+  for (int i = 0; i < str.length(); i++){
+    result += str.substring(0, i+1);
+  } return result;
+}
+
+
 // to be continued ...
   
 }
