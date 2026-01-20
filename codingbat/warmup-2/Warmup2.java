496006a5a8cb1323b2c6fb7dd729035fcc6696a2
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-20 20:48:46 +0700

    problem 16 solved

--- codingbat/warmup-2/Warmup2.java
+++ codingbat/warmup-2/Warmup2.java
@@ -219,6 +219,18 @@ public class Warmup2 {
     if (nums[i] == 6 && (nums[i+1] == 6 || nums[i+1] == 7)) count++;
   } return count;
 }
+
+// =============================
+//       16. noTriples
+// =============================
+
+//My Solution 
+  public boolean noTriples(int[] nums) {
+  for ( int i = 0; i < nums.length-2; i ++ ){
+    if ((nums[i] == nums[i+1]) && (nums[i] == nums[i+2])) return false;
+  }
+  return true;
+}
   
 // to be continued ...
   
