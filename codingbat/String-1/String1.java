6ba49527244319921e6c4d10f4dbeaf49c9f5efa
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-21 12:16:38 +0700

    problem 1 solved

---
+++ codingbat/String-1/String1.java
@@ -0,0 +1,12 @@
+public class String1 {
+
+// =============================
+//    1. helloName
+// =============================
+
+//My Solution 
+  public String helloName(String name) {
+  return "Hello " + name + "!";
+}
+  // to be continued ... 
+}
